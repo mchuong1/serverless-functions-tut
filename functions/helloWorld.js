601202e7e97155750c80db80eab74c039e2e826a
Matthew Chuong <mstchuong@yahoo.com>
2021-06-12 19:10:12 -0400

    Setup serverless function

---
+++ functions/helloWorld.js
@@ -0,0 +1,7 @@
+//Every serverless function needs this
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({message: 'Hello World!!!'})
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [message, setMessage] = useState('');
+  const fetchData = async () => {
+    const results = await axios.get('/.netlify/functions/helloWorld');
+    setMessage(results.data.message);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{message}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
